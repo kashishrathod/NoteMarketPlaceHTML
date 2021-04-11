bbd05c64cc0d686b265326b94d4edbc694d37dea
kashishrathod <53112438+kashishrathod@users.noreply.github.com>
2021-04-11 14:15:13 +0530

    redirect to dashboard page on click navbar logo

--- MVC 3/notes-project/notes-admin/nav-admin.php
+++ MVC 3/notes-project/notes-admin/nav-admin.php
@@ -15,7 +15,7 @@ while ($row = mysqli_fetch_assoc($query_pic)) {
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light  bg-light fixed-top  general-navbar">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="dashboard.php">
         <img src="img/pre-login/logo.png" alt="Logo" />
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
