67b0495b43262f99d3f1f9c101d6bdc1e2201718
kashishrathod <53112438+kashishrathod@users.noreply.github.com>
2021-04-14 19:49:01 +0530

    added header location

--- MVC 3/notes-project/Notes/check.php
+++ MVC 3/notes-project/Notes/check.php
@@ -1,8 +1,11 @@
 <?php
 
 include "db_connection.php";
-$id = mysqli_real_escape_string($conn, $_GET['userid']);
-mysqli_query($conn, "update users set isemailverified=1 where userid = $id");
+if(isset($_GET['userid'])){
+    $userid = $_GET['userid'];
+}
+mysqli_query($conn, "update users set isemailverified=1 where userid = $userid");
+header("Location:login.php");
 
 
 ?>
